6205ba2668f98d6cb9ca4db9d6560ebf70ab6c6f
David <69691740+piroworkz@users.noreply.github.com>
2025-08-31 13:31:18 -0600

    Test: Add End-to-End Test and HiltTestRunner
    This commit introduces an end-to-end test class and a custom HiltTestRunner.
    
    **End-to-End Test:**
    - Added `TmdbAppEndToEndTest.kt` in `app/src/androidTest/kotlin/com/davidluna/tmdb/app/main_ui`.
    - This class sets up an `AndroidComposeRule` for `MainActivity` to facilitate UI testing.
    
    **HiltTestRunner:**
    - Added `HiltTestRunner.kt` in `app/src/androidTest/kotlin/com/davidluna/tmdb/app`.
    - This custom runner extends `AndroidJUnitRunner` and overrides `newApplication` to use `HiltTestApplication`. This is necessary for Hilt integration in Android instrumented tests.
    
    **Build Configuration:**
    - Updated `Constants.kt` in `build-logic/convention` to change `HILT_TEST_RUNNER` from `androidx.test.runner.AndroidJUnitRunner` to the new `com.davidluna.tmdb.app.HiltTestRunner`.

---
+++ app/src/androidTest/kotlin/com/davidluna/tmdb/app/main_ui/TmdbAppEndToEndTest.kt
@@ -0,0 +1,31 @@
+package com.davidluna.tmdb.app.main_ui
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class TmdbAppEndToEndTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setup(){
+        hiltRule.inject()
+    }
+
+    @Test
+    fun test_whenAppIsLaunchedForFirstTime_ShouldShowPermissionsPromptScreenAfterSplash(): Unit = composeRule.run {
+
+    }
+}
