d8d54a08b926aa9f7c58c0dc8f6300b630c1236c
David <69691740+piroworkz@users.noreply.github.com>
2026-01-02 13:32:24 -0600

    Use MockK in local favorites test

--- feature/media/media_framework/src/test/kotlin/com/davidluna/tmdb/media_framework/data/local/storage/LocalFavoritesDataSourceTest.kt
+++ feature/media/media_framework/src/test/kotlin/com/davidluna/tmdb/media_framework/data/local/storage/LocalFavoritesDataSourceTest.kt
@@ -1,18 +1,27 @@
 package com.davidluna.tmdb.media_framework.data.local.storage
 
-import com.davidluna.tmdb.media_framework.data.local.database.dao.FavoritesDaoSpy
 import com.davidluna.tmdb.media_framework.data.local.database.entities.favorites.RoomFavorite
+import com.davidluna.tmdb.media_framework.data.local.database.dao.FavoritesDao
 import com.davidluna.tmdb.test_shared.rules.CoroutineTestRule
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit4.MockKRule
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
 import org.junit.Rule
 import org.junit.Test
 
 class LocalFavoritesDataSourceTest {
 
     @get:Rule(order = 1)
+    val mockkRule = MockKRule(this)
+
+    @get:Rule(order = 2)
     val coroutineRule = CoroutineTestRule()
+
+    @MockK
+    private lateinit var favoritesDao: FavoritesDao
 
     private val favorite = RoomFavorite(
         category = "MOVIE_POPULAR",
@@ -24,25 +33,28 @@ class LocalFavoritesDataSourceTest {
     @Test
     fun `GIVEN favorite does not exist WHEN toggled THEN favorite is stored and returns true`() =
         coroutineRule.scope.runTest {
-            val favoritesDao = FavoritesDaoSpy()
+            coEvery { favoritesDao.getFavorite(favorite.id, favorite.category) } returns null
+            coEvery { favoritesDao.upsertFavorite(favorite) } returns Unit
             val sut = LocalFavoritesDataSource(favoritesDao)
 
             val actual = sut(favorite).getOrNull()
 
             assertEquals(true, actual)
-            assertEquals(favorite, favoritesDao.getFavorite(favorite.id, favorite.category))
+            coVerify(exactly = 1) { favoritesDao.upsertFavorite(favorite) }
+            coVerify(exactly = 0) { favoritesDao.deleteFavorite(favorite.id, favorite.category) }
         }
 
     @Test
     fun `GIVEN favorite exists WHEN toggled THEN favorite is removed and returns false`() =
         coroutineRule.scope.runTest {
-            val favoritesDao = FavoritesDaoSpy()
-            favoritesDao.upsertFavorite(favorite)
+            coEvery { favoritesDao.getFavorite(favorite.id, favorite.category) } returns favorite
+            coEvery { favoritesDao.deleteFavorite(favorite.id, favorite.category) } returns 1
             val sut = LocalFavoritesDataSource(favoritesDao)
 
             val actual = sut(favorite).getOrNull()
 
             assertEquals(false, actual)
-            assertNull(favoritesDao.getFavorite(favorite.id, favorite.category))
+            coVerify(exactly = 1) { favoritesDao.deleteFavorite(favorite.id, favorite.category) }
+            coVerify(exactly = 0) { favoritesDao.upsertFavorite(favorite) }
         }
 }
