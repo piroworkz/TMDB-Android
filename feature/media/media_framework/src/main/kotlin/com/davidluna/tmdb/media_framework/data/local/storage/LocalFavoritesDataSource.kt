585a5811d4caa21fb6eb314a4f9f2cc2296c7c0d
David <69691740+piroworkz@users.noreply.github.com>
2026-01-02 14:43:23 -0600

    Wire toggle favorites use case

--- feature/media/media_framework/src/main/kotlin/com/davidluna/tmdb/media_framework/data/local/storage/LocalFavoritesDataSource.kt
+++ feature/media/media_framework/src/main/kotlin/com/davidluna/tmdb/media_framework/data/local/storage/LocalFavoritesDataSource.kt
@@ -3,13 +3,18 @@ package com.davidluna.tmdb.media_framework.data.local.storage
 import arrow.core.Either
 import com.davidluna.tmdb.core_domain.entities.AppError
 import com.davidluna.tmdb.core_domain.entities.tryCatch
+import com.davidluna.tmdb.media_domain.entities.FavoriteItem
+import com.davidluna.tmdb.media_domain.usecases.ToggleFavorite
 import com.davidluna.tmdb.media_framework.data.local.database.dao.FavoritesDao
 import com.davidluna.tmdb.media_framework.data.local.database.entities.favorites.RoomFavorite
 import javax.inject.Inject
 
 class LocalFavoritesDataSource @Inject constructor(
     private val favoritesDao: FavoritesDao,
-) {
+) : ToggleFavorite {
+    override suspend fun invoke(item: FavoriteItem): Either<AppError, Boolean> =
+        toggleFavorite(item.toRoomFavorite())
+
     suspend fun toggleFavorite(favorite: RoomFavorite): Either<AppError, Boolean> = tryCatch {
         val storedFavorite = favoritesDao.getFavorite(favorite.id, favorite.category)
         if (storedFavorite == null) {
@@ -20,4 +25,11 @@ class LocalFavoritesDataSource @Inject constructor(
             false
         }
     }
+
+    private fun FavoriteItem.toRoomFavorite() = RoomFavorite(
+        category = mediaType.name,
+        id = id,
+        posterPath = posterPath,
+        title = title,
+    )
 }
