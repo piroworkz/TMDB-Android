e9ccd950f26ea9127af9d566fe28a76cd5d1880b
David <69691740+piroworkz@users.noreply.github.com>
2026-01-04 09:31:55 -0600

    Adjust ClearFavorites return type

--- feature/media/media_domain/src/main/kotlin/com/davidluna/tmdb/media_domain/usecases/ClearFavorites.kt
+++ feature/media/media_domain/src/main/kotlin/com/davidluna/tmdb/media_domain/usecases/ClearFavorites.kt
@@ -3,4 +3,4 @@ package com.davidluna.tmdb.media_domain.usecases
 import arrow.core.Either
 import com.davidluna.tmdb.core_domain.entities.AppError
 
-fun interface ClearFavorites : suspend () -> Either<AppError, Boolean>
+fun interface ClearFavorites : suspend () -> Either<AppError, Unit>
