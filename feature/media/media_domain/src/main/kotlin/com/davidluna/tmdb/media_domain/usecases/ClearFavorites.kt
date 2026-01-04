5ec652c1ac8713d0367fca8a5a31f2b99331ec2d
David <69691740+piroworkz@users.noreply.github.com>
2026-01-04 09:30:37 -0600

    Add ClearFavorites use case interface

---
+++ feature/media/media_domain/src/main/kotlin/com/davidluna/tmdb/media_domain/usecases/ClearFavorites.kt
@@ -0,0 +1,6 @@
+package com.davidluna.tmdb.media_domain.usecases
+
+import arrow.core.Either
+import com.davidluna.tmdb.core_domain.entities.AppError
+
+fun interface ClearFavorites : suspend () -> Either<AppError, Boolean>
